91044fc6f659a6f5638b831e29f07b9aaf880c2d
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-10-12 17:11:22 +0100

    Initializing sample test stage
    Creating possible tests based on my expectations

---
+++ LCATest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LCATest {
+	
+//	Prior to starting my code I am going to want to test all expected areas. 
+//	Need to test the LCA for two given nodes
+//	    ______5______
+//     /             \
+//   __5__          __3__
+//  /     \        /     \
+// 6       2      0       8
+//        / \
+//       7   4
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
