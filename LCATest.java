95725732385996f0d9cdb77b7a8b9c757100ab4a
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-10-12 17:55:52 +0100

    Tree built
    Tree designed according to the visual example

--- LCATest.java
+++ LCATest.java
@@ -17,9 +17,21 @@ public class LCATest {
 //        / \
 //       7   4
 	
+	
+	LCA tree = new LCA();
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+        tree.root = new Node(3);
+        tree.root.left = new Node(5);
+        tree.root.right = new Node(1);
+        tree.root.left.left = new Node(6);
+        tree.root.left.right = new Node(2);
+        tree.root.right.left = new Node(0);
+        tree.root.right.right = new Node(8);
+        tree.root.left.right.left = new Node(7);
+        tree.root.left.right.right = new Node(4);
 	}
 
 }
