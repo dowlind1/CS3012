7f6716e9c75493170f4fdc2b36206cdd156e407a
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-10-14 18:11:45 +0100

    Creating function from test class
    Still undecided on methodology but aware will need a class matching the criteria from the test class

--- LCA.java
+++ LCA.java
@@ -1,8 +1,11 @@
 
 public class LCA {
 
-	Node root; //Calling in the node class
+	Node root;//Calling in the node class
 	
+	public int findLCA(int v, int w){
+		return -1;
+	}
 //	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
 //
