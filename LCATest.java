850a2ebb45b4880778c483a6bc6afc78a6ebe2a6
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-10-14 17:43:27 +0100

    Testing Same elements and nodes for LCA and some cleansing of the rest of the code
    Specififed some of the code a bit better and placed in appropriate testing category.
    
    Tested the root and some same elements to show that the LCA should still be of that value

--- LCATest.java
+++ LCATest.java
@@ -35,24 +35,22 @@ public class LCATest {
 		tree.root.left.right.right = new Node(4);
 
 		//testing that the LCA function is working when I implement it, order should not matter
-		assertEquals(3 ,tree.findLCA(1, 5));
-		assertEquals(3 ,tree.findLCA(5, 1));//order changed
-		assertEquals(3 ,tree.findLCA(3, 5));//checked that LCA can still be parent
-		assertEquals(3 ,tree.findLCA(1, 3));
-
+		assertEquals("LCA = 3",3 ,tree.findLCA(1, 5));
+		assertEquals("LCA = 3",3 ,tree.findLCA(5, 1));//change the order around
+		
 		//widening the parameters
-		assertEquals(3 ,tree.findLCA(6, 1));
-		assertEquals(2 ,tree.findLCA(7, 4));
-		assertEquals(1 ,tree.findLCA(0, 8));
+		assertEquals("LCA = 3",3 ,tree.findLCA(6, 1));
+		assertEquals("LCA = 2",2 ,tree.findLCA(7, 4));
+		assertEquals("LCA = 1",1 ,tree.findLCA(0, 8));
 	}
 
 	@Test
 	public void testConstructor() {
 
-		Node rootNode = new Node(3);//only the root node 
-		assertEquals(null ,rootNode.left);
-		asserEquals(null ,rootNode.right);
-		assertEquals(3 ,rootNode.value);
+		Node rootNode = new Node(0);//only the root node 
+		assertEquals("Only one node, therefor left side is equal to null",null ,rootNode.left);
+		assertEquals("Only one node, therefor right side is equal to null",null ,rootNode.right);
+		assertEquals("Value = 0",0 ,rootNode.value);
 	}
 
 	@Test
@@ -91,8 +89,33 @@ public class LCATest {
 		assertEquals("Will equal to -1 if does not exist",-1 ,tree.findLCA(1, 23));
 		//neither elements exist
 		assertNotEquals("Will equal to -2 if neither exist",-2,tree.findLCA(9,10));
+		assertEquals("LCA = null",3 ,tree.findLCA(3, 3));
 	}
 	
+	@Test
+	public void testSameNode(){
+		
+		tree.root = new Node(3);
+		tree.root.left = new Node(5);
+		tree.root.right = new Node(1);
+		tree.root.left.left = new Node(6);
+		tree.root.left.right = new Node(2);
+		tree.root.right.left = new Node(0);
+		tree.root.right.right = new Node(8);
+		tree.root.left.right.left = new Node(7);
+		tree.root.left.right.right = new Node(4);
+		
+		//same root element, LCA is the element and the root
+		assertEquals("LCA = null",3 ,tree.findLCA(3, 3));
+		
+		//one root element and another, LCA is one of the elements and the root 
+		assertEquals(3 ,tree.findLCA(3, 5));//checked that LCA can still be parent
+		assertEquals(3 ,tree.findLCA(1, 3));
+		
+		//same non root element, LCA is the element itself
+		assertEquals("LCA = 1",1 ,tree.findLCA(1, 1));
+		assertEquals("LCA = 5",5 ,tree.findLCA(5, 5));
+	}
 	
 
 }
