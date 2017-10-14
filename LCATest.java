2b8e1e8b13f83342bd126b18b8d6081158d45e32
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-10-14 17:12:32 +0100

    Testing for non existing nodes within
    Going to give non existing nodes a value of -1. Meaning if there is more than one checked for it will relate to the amount missing. ie if two missing, a value of -2 will apear

--- LCATest.java
+++ LCATest.java
@@ -23,6 +23,7 @@ public class LCATest {
 	@Test
 	public void testTree() {
 
+		// here I am building a tree with values
 		tree.root = new Node(3);
 		tree.root.left = new Node(5);
 		tree.root.right = new Node(1);
@@ -33,11 +34,13 @@ public class LCATest {
 		tree.root.left.right.left = new Node(7);
 		tree.root.left.right.right = new Node(4);
 
+		//testing that the LCA function is working when I implement it, order should not matter
 		assertEquals(3 ,tree.findLCA(1, 5));
 		assertEquals(3 ,tree.findLCA(5, 1));//order changed
 		assertEquals(3 ,tree.findLCA(3, 5));//checked that LCA can still be parent
 		assertEquals(3 ,tree.findLCA(1, 3));
 
+		//widening the parameters
 		assertEquals(3 ,tree.findLCA(6, 1));
 		assertEquals(2 ,tree.findLCA(7, 4));
 		assertEquals(1 ,tree.findLCA(0, 8));
@@ -48,7 +51,7 @@ public class LCATest {
 
 		Node rootNode = new Node(3);//only the root node 
 		assertEquals(null ,rootNode.left);
-		assertEquals(null ,rootNode.right);
+		asserEquals(null ,rootNode.right);
 		assertEquals(3 ,rootNode.value);
 	}
 
@@ -59,10 +62,10 @@ public class LCATest {
 
 	@Test
 	public void testSmallTree() {
-
+		//create a new small tree
 		tree.root = new Node(3);
 		tree.root.left = new Node(5);
-
+		
 		assertEquals(3 ,tree.findLCA(3, 5));
 		assertEquals(3 ,tree.findLCA(5, 3));
 		assertEquals(5 ,tree.root.left.value);
@@ -70,5 +73,26 @@ public class LCATest {
 		//assertEquals(null ,tree.root.right.value);
 		assertEquals(3 ,tree.root.value);
 	}
+	
+	@Test
+	public void testNonExistingNodes(){
+		
+		tree.root = new Node(3);
+		tree.root.left = new Node(5);
+		tree.root.right = new Node(1);
+		tree.root.left.left = new Node(6);
+		tree.root.left.right = new Node(2);
+		tree.root.right.left = new Node(0);
+		tree.root.right.right = new Node(8);
+		tree.root.left.right.left = new Node(7);
+		tree.root.left.right.right = new Node(4);
+		
+		//one element doesn't exist
+		assertEquals("Will equal to -1 if does not exist",-1 ,tree.findLCA(1, 23));
+		//neither elements exist
+		assertNotEquals("Will equal to -2 if neither exist",-2,tree.findLCA(9,10));
+	}
+	
+	
 
 }
