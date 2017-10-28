591d2a07a7d4a8c66a042d019c6a6e9b75db7375
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-10-28 14:36:17 +0100

    Test the constructor
    Using two nodes as examples to show their similarity in getting set up and also that they have no connections as of yet

--- NodeTest.java
+++ NodeTest.java
@@ -8,7 +8,16 @@ public class NodeTest{
   
   @Test
   public void constructorTest(){//test the constructor is being set up right
-  
+    //took two nodes to show no connection has been made 
+    //also that the constructor will set them up the way I expect to need them
+    assertEquals("Test the value in node a","a" ,a.value);
+		assertEquals("Test the value in node b","b" ,b.value);
+		assertEquals("Confirm a is not connected to anything yet",0 ,a.outdegree);
+		assertEquals("Confirm b is not connected to anything yet",0 ,b.outdegree);
+		assertEquals("Confirm a is not connected to anything yet",0 ,a.indegree);
+		assertEquals("Confirm b is not connected to anything yet",0 ,b.indegree);
+		assertEquals("Confirm a is not connected to anything yet",true ,a.edgesTo.isEmpty());
+		assertEquals("Confirm b is not connected to anything yet",true ,b.edgesTo.isEmpty());
   }
   
   /**
